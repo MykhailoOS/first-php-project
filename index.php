4ccbac61a7b8cec1b1869bc30102c741384be6cc
MykhailP <134710332+MykhailoOS@users.noreply.github.com>
2024-03-01 11:21:31 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title><?= "DOC" ?></title>
+</head>
+<body>
+<h1>My index page</h1><br>
+<?php
+echo "Привет!"
+?>
+<br>
+
+</body>
+</html>
